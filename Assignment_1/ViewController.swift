a604e388f5de6e2122cf803835fe894680269164
benwhardy <benwhardy@gmail.com>
2023-05-18 11:09:19 -0500

    Initial Commit

---
+++ Assignment_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_1
+//
+//  Created by Ben Hardy on 5/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
